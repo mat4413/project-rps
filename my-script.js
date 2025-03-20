ab2cbd6ad00d09493c11f86b90533f07686ba1e0
matias <jmatiasartecona@gmail.com>
2025-03-20 14:28:59 -0300

    Add input validation for invalid player choices in getHumanChoice

--- my-script.js
+++ my-script.js
@@ -31,55 +31,65 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
 
-    if (humanSelection === rock && computerSelection === paper){
-        computerScore += 1;
-        alert("You lose! Paper beats Rock.");
-        alert(`Human: ${humanScore}\n
-Machine: ${computerScore}`);
-    } else if (humanSelection === rock && computerSelection === scissor){
-        humanScore += 1;
-        alert("You Win! Rock beats Scissor.");
-        alert(`Human: ${humanScore}\n
-Machine: ${computerScore}`);
-    } else if (humanSelection === rock && computerSelection === rock){
-    alert("Both chose rock, play again.");
-    alert(`Human: ${humanScore}\n
-Machine: ${computerScore}`);
+    let resultMessage = ""
 
-    } else if (humanSelection === paper && computerSelection === scissor){
+    if (humanChoice === rock && computerChoice === paper){
         computerScore += 1;
-        alert("You lose! Scissor beats Paper.");
-        alert(`Human: ${humanScore}\n
-Machine: ${computerScore}`);
-    } else if (humanSelection === paper && computerSelection === rock){
+        resultMessage = "You lose! Paper beats Rock.";
+       
+    } else if (humanChoice === rock && computerChoice === scissor){
         humanScore += 1;
-        alert("You Win! Paper beats Rock.");
-        alert(`Human: ${humanScore}\n
-Machine: ${computerScore}`);
-    } else if (humanSelection === paper && computerSelection === paper){
-    alert("Both chose Paper, play again.");
-    alert(`Human: ${humanScore}\n
-Machine: ${computerScore}`);
+       resultMessage = "You Win! Rock beats Scissor.";
+       
+    } else if (humanChoice === rock && computerChoice === rock){
+    resultMessage = "Both chose rock, play again.";
+  
 
-    } else if (humanSelection === scissor && computerSelection === rock){
+    } else if (humanChoice === paper && computerChoice === scissor){
         computerScore += 1;
-        alert("You lose! Rock beats Scissor.");
-        alert(`Human: ${humanScore}\n
-Machine: ${computerScore}`);
-    } else if (humanSelection === scissor && computerSelection === paper){
+        resultMessage = alert("You lose! Scissor beats Paper.");
+        
+    } else if (humanChoice === paper && computerChoice === rock){
         humanScore += 1;
-        alert("You Win! Scissor beats Papper.");
-        alert(`Human: ${humanScore}\n
-Machine: ${computerScore}`);
-    } else if (humanSelection === scissor && computerSelection === scissor){
-    alert("Both chose Scissor, play again.");
-    alert(`Human: ${humanScore}\n
-Machine: ${computerScore}`);
-}
+       resultMessage = "You Win! Paper beats Rock.";
+       
+    } else if (humanChoice === paper && computerChoice === paper){
+    resultMessage = "Both chose Paper, play again.";
+    
+
+    } else if (humanChoice === scissor && computerChoice === rock){
+        computerScore += 1;
+        resultMessage = "You lose! Rock beats Scissor.";
+       
+    } else if (humanChoice === scissor && computerChoice === paper){
+        humanScore += 1;
+        resultMessage = "You Win! Scissor beats Papper.";
+       
+    } else if (humanChoice === scissor && computerChoice === scissor){
+   resultMessage = "Both chose Scissor, play again.";
+    
+    }
+
+alert(resultMessage);
+alert(`Human: ${humanScore}\nMachine: ${computerScore}`);
+
+
 
 }
+
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 console.log(playRound(humanSelection, computerSelection));
+
+
+
+
+
+
+
+
+
+
+
